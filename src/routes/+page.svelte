8bb7b82b3a910df7a082b891ac5d6537a50b7bfc
JYLN <14062951+JYLN@users.noreply.github.com>
2023-08-03 15:32:02 -0600

    Build info cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,36 @@
 	</figure>
 </section>
 
+<section class="info-cards">
+	<article class="card">
+		<div class="card-header">
+			<i class="bx bxs-wallet bx-fw" />
+		</div>
+		<div class="card-text">
+			<h3>cheaper</h3>
+			<p>than traditional methods</p>
+		</div>
+	</article>
+	<article class="card">
+		<div class="card-header">
+			<i class="bx bxs-timer bx-fw" />
+		</div>
+		<div class="card-text">
+			<h3>faster</h3>
+			<p>than traditional methods</p>
+		</div>
+	</article>
+	<article class="card">
+		<div class="card-header">
+			<i class="bx bxs-hard-hat bx-fw" />
+		</div>
+		<div class="card-text">
+			<h3>Safer</h3>
+			<p>than traditional methods</p>
+		</div>
+	</article>
+</section>
+
 <style lang="postcss">
 	.hero {
 		@apply container mx-auto flex flex-col-reverse items-center justify-between gap-2 sm:flex-row;
@@ -27,5 +57,25 @@
 
 	section {
 		@apply mb-8;
+	}
+
+	.info-cards {
+		@apply container mx-auto flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-5 md:gap-12;
+	}
+
+	.card {
+		@apply bg-hydragreen-500 p-8 text-center text-white sm:basis-1/3;
+	}
+
+	.card-header {
+		@apply mb-2 text-5xl md:text-6xl;
+	}
+
+	.card-text h3 {
+		@apply -mb-1 font-inter text-xl font-bold uppercase md:text-2xl lg:text-4xl;
+	}
+
+	.card-text p {
+		@apply text-lg;
 	}
 </style>
